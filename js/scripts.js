d1fcadf62441b5b9e495feaced33f4c3562e8f2a
Parking-Team <79455375+Parking-Team@users.noreply.github.com>
2021-02-22 23:26:41 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+/*var w = window.innerWidth;
+var nav = document.getElementById("navbar");
+if( w > '1368' ){
+    nav.classList.add("size-lga");
+}else if( w >= '992' ){
+    nav.classList.add("size-mda");
+}else if( w >= '768' ){
+    nav.classList.add("size-sma");
+}*/
