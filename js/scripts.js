5fde4f1b6bebfc9f73ef1027f91c161a7d2e2f4c
Parking-Team <79455375+Parking-Team@users.noreply.github.com>
2021-02-24 01:17:48 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,58 @@
-/*var w = window.innerWidth;
-var nav = document.getElementById("navbar");
-if( w > '1368' ){
-    nav.classList.add("size-lga");
-}else if( w >= '992' ){
-    nav.classList.add("size-mda");
-}else if( w >= '768' ){
-    nav.classList.add("size-sma");
-}*/
+
+var h = document.getElementById("h");
+var a = document.getElementById("a");
+var s = document.getElementById("s");
+var p = document.getElementById("p");
+var c = document.getElementById("c");
+
+function home(){
+    h.classList.add("active");
+    a.classList.remove("active");
+    s.classList.remove("active");
+    p.classList.remove("active");
+    c.classList.remove("active");
+}
+
+function about(){
+    a.classList.add("active");
+    h.classList.remove("active");
+    s.classList.remove("active");
+    p.classList.remove("active");
+    c.classList.remove("active");
+}
+
+function services(){
+    s.classList.add("active");
+    a.classList.remove("active");
+    h.classList.remove("active");
+    p.classList.remove("active");
+    c.classList.remove("active");
+}
+
+function projects(){
+    p.classList.add("active");
+    a.classList.remove("active");
+    s.classList.remove("active");
+    h.classList.remove("active");
+    c.classList.remove("active");
+}
+
+function contact(){
+    c.classList.add("active");
+    a.classList.remove("active");
+    s.classList.remove("active");
+    p.classList.remove("active");
+    h.classList.remove("active");
+}
+
+function closeContact(){
+    if(h.classList.contains("acitve")){
+        home();
+    }else if(a.classList.contains("acitve")){
+        about();
+    }else if(s.classList.contains("acitve")){
+        services();
+    }else if(p.classList.contains("acitve")){
+        projects();
+    }
+}
