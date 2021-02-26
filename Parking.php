3f39e19044d65203077f336d81c26e8754ef1eb5
Parking-Team <79455375+Parking-Team@users.noreply.github.com>
2021-02-26 13:11:48 +0200

    Update Parking.php

--- Parking.php
+++ Parking.php
@@ -265,7 +265,7 @@
         <center><h2>About Us</h2></center>
         <br>
         <center>
-            <p style="width: auto;">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you. This is a great space to write long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide. Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors.</p>    
+            <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you. This is a great space to write long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide. Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors.</p>    
         </center>
         
         </div>
